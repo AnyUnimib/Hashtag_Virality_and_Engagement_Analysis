ee816d115ad9a30feb56d80fc73c3f6bc79570c0
AnyUnimib <a.das3@campus.unimib.it>
2026-08-23 21:01:44 +0200

    Add files via upload

---
+++ mock.py
@@ -0,0 +1,82 @@
+ # Twitter API Responses For Hashtag Queries
+
+HappyChristmas = {
+    "data": [
+        {
+            "id": "1890817569742848501",
+            "text": "RT @LOVEONE_staff: スペシャルフリーイベント\n「RUNO'S BIRTHDAY HANGOUT」\n@イオンモール幕張新都心\n\n寒い中のご来場、そして #飯塚瑠乃 の誕生日をお祝いいただきありがとうございました🦋🎂✨\n\nセトリは飯塚考案💭\n3rd ALBUM収録…",
+            "edit_history_tweet_ids": [
+                "1890817569742848501"
+            ]
+        },
+        {
+            "id": "1890811487226048615",
+            "text": "RT @LOVEONE_staff: スペシャルフリーイベント\n「RUNO'S BIRTHDAY HANGOUT」\n@イオンモール幕張新都心\n\n寒い中のご来場、そして #飯塚瑠乃 の誕生日をお祝いいただきありがとうございました🦋🎂✨\n\nセトリは飯塚考案💭\n3rd ALBUM収録…",
+            "edit_history_tweet_ids": [
+                "1890811487226048615"
+            ]
+        },
+        {
+            "id": "1890808693735727293",
+            "text": "RT @LOVEONE_staff: スペシャルフリーイベント\n「RUNO'S BIRTHDAY HANGOUT」\n@イオンモール幕張新都心\n\n寒い中のご来場、そして #飯塚瑠乃 の誕生日をお祝いいただきありがとうございました🦋🎂✨\n\nセトリは飯塚考案💭\n3rd ALBUM収録…",
+            "edit_history_tweet_ids": [
+                "1890808693735727293"
+            ]
+        },
+        {
+            "id": "1890805746868117669",
+            "text": "RT @LOVEONE_staff: スペシャルフリーイベント\n「RUNO'S BIRTHDAY HANGOUT」\n@イオンモール幕張新都心\n\n寒い中のご来場、そして #飯塚瑠乃 の誕生日をお祝いいただきありがとうございました🦋🎂✨\n\nセトリは飯塚考案💭\n3rd ALBUM収録…",
+            "edit_history_tweet_ids": [
+                "1890805746868117669"
+            ]
+        },
+        {
+            "id": "1890789802036867414",
+            "text": "RT @LOVEONE_staff: スペシャルフリーイベント\n「RUNO'S BIRTHDAY HANGOUT」\n@イオンモール幕張新都心\n\n寒い中のご来場、そして #飯塚瑠乃 の誕生日をお祝いいただきありがとうございました🦋🎂✨\n\nセトリは飯塚考案💭\n3rd ALBUM収録…",
+            "edit_history_tweet_ids": [
+                "1890789802036867414"
+            ]
+        },
+        {
+            "id": "1890785687495983447",
+            "text": "RT @LOVEONE_staff: スペシャルフリーイベント\n「RUNO'S BIRTHDAY HANGOUT」\n@イオンモール幕張新都心\n\n寒い中のご来場、そして #飯塚瑠乃 の誕生日をお祝いいただきありがとうございました🦋🎂✨\n\nセトリは飯塚考案💭\n3rd ALBUM収録…",
+            "edit_history_tweet_ids": [
+                "1890785687495983447"
+            ]
+        },
+        {
+            "id": "1890778784254636151",
+            "text": "RT @LOVEONE_staff: スペシャルフリーイベント\n「RUNO'S BIRTHDAY HANGOUT」\n@イオンモール幕張新都心\n\n寒い中のご来場、そして #飯塚瑠乃 の誕生日をお祝いいただきありがとうございました🦋🎂✨\n\nセトリは飯塚考案💭\n3rd ALBUM収録…",
+            "edit_history_tweet_ids": [
+                "1890778784254636151"
+            ]
+        },
+        {
+            "id": "1890774533587480898",
+            "text": "RT @LOVEONE_staff: スペシャルフリーイベント\n「RUNO'S BIRTHDAY HANGOUT」\n@イオンモール幕張新都心\n\n寒い中のご来場、そして #飯塚瑠乃 の誕生日をお祝いいただきありがとうございました🦋🎂✨\n\nセトリは飯塚考案💭\n3rd ALBUM収録…",
+            "edit_history_tweet_ids": [
+                "1890774533587480898"
+            ]
+        },
+        {
+            "id": "1890769238492631087",
+            "text": "RT @LOVEONE_staff: スペシャルフリーイベント\n「RUNO'S BIRTHDAY HANGOUT」\n@イオンモール幕張新都心\n\n寒い中のご来場、そして #飯塚瑠乃 の誕生日をお祝いいただきありがとうございました🦋🎂✨\n\nセトリは飯塚考案💭\n3rd ALBUM収録…",
+            "edit_history_tweet_ids": [
+                "1890769238492631087"
+            ]
+        },
+        {
+            "id": "1890767280516071544",
+            "text": "RT @LOVEONE_staff: スペシャルフリーイベント\n「RUNO'S BIRTHDAY HANGOUT」\n@イオンモール幕張新都心\n\n寒い中のご来場、そして #飯塚瑠乃 の誕生日をお祝いいただきありがとうございました🦋🎂✨\n\nセトリは飯塚考案💭\n3rd ALBUM収録…",
+            "edit_history_tweet_ids": [
+                "1890767280516071544"
+            ]
+        }
+    ],
+    "meta": {
+        "newest_id": "1890817569742848501",
+        "oldest_id": "1890767280516071544",
+        "result_count": 10,
+        "next_token": "b26v89c19zqg8o3frrcq7f0w0hvb4ppoaezvu3ad66n3x"
+    }
+}
